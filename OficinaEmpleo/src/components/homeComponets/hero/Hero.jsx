17f351e8400d48184efea756a43e76185c5c11cf
PradoLucas14 <lucasnahuelprado0@gmail.com>
2024-12-06 09:33:40 -0300

    se realizaron modificaciones en la hoja de estilos del componente hero

--- OficinaEmpleo/src/components/homeComponets/hero/Hero.jsx
+++ OficinaEmpleo/src/components/homeComponets/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.css';
 import "./Hero.css" 
 
 function Hero() {
@@ -8,8 +9,17 @@ function Hero() {
             <div className="hero-columns">
             {/* Columna con texto */}
             <div className="column column-text">
-                <h2>Bienvenidos</h2>
-                <p>Este es un ejemplo de texto para la columna izquierda.</p>
+                <h2>Bienvenidos a la</h2>
+                <h1>Oficina de empleo de Alderetes.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis odit dolores reprehenderit.</p>
+                <div className='redesHero'>
+                    <button className='RedHero'>
+                        <i className="fab fa-instagram"></i><p>Instagram</p>
+                    </button>
+                    <button className='RedHero'>
+                        <i className="fab fa-facebook"></i><p>Facebook</p>
+                    </button>
+                </div>
             </div>
             {/* Columna vacía */}
             <div className="column column-empty"></div>
