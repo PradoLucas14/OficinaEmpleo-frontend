6e4e5b16f9dfdfdfcc666e8137be87d8e911bf65
PradoLucas14 <lucasnahuelprado0@gmail.com>
2024-12-06 11:59:44 -0300

    se creo el componente de layout NavBar y su respectiva hoja de estilos

--- OficinaEmpleo/src/layout/navbar/NavBar.jsx
+++ OficinaEmpleo/src/layout/navbar/NavBar.jsx
@@ -1,20 +1,30 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../../assets/img/logo2.png"
+import "./NavBar.css"
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className='brand'>
+            <div className='imgBrand'>
+                <img src={logo} alt="logo empleo" />
+            </div>
+            <div className='textBrand'>
+                <h6>OFICINA DE EMPLEO</h6>
+                <p>Alderetes</p>
+            </div>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto ms-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Conocenos</Nav.Link>
-            <Nav.Link href="#link">Galeria</Nav.Link>
-            <Nav.Link href="#link">Acceso</Nav.Link>
+        <div className='ms-auto'></div>
+            <Nav className="me-auto">
+                <Nav.Link href="#link">Inicio</Nav.Link>
+                <Nav.Link href="#link">Conocenos</Nav.Link>
+                <Nav.Link href="#link">Galeria</Nav.Link>
+                <Nav.Link href="#link">Acceso</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
