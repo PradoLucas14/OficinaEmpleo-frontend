7242b9798f1d6442cbe5f0cb233b811757456542
PradoLucas14 <lucasnahuelprado0@gmail.com>
2024-12-06 12:55:09 -0300

    se modificaron los estilos del componente NavBar ademas de agregarle la imagen y el texto correspondiente al elemento brand

--- OficinaEmpleo/src/layout/navbar/NavBar.jsx
+++ OficinaEmpleo/src/layout/navbar/NavBar.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import logo from "../../assets/img/logo2.png"
+import logo from "../../assets/img/logo.svg"
 import "./NavBar.css"
 
 function BasicExample() {
